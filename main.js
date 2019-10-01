a5f2c60442bc423da83cc2cc625ea4f49de3191b
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-10-01 15:22:57 -0400

    finalized

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 function fizzy(num) {
-
+  let response = ' ';
+  const fizz = (num%3===0);
+  const buzz = (num%5===0);
+if ( fizz && buzz) {
+response = 'FizzBuzz'}
+else if ( fizz && !buzz){
+response = 'Fizz'}
+else if ( !fizz && buzz){
+response = 'Buzz'}
+else if (!fizz && !buzz){
+response = num}
+return response;
 }
+
+
 
 module.exports = {
   fizzy,
